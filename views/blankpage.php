004fa5036ee0bc9430bf60f45ae6e2919e773ba1
evanescence <atieh@evanescence-pc.gditac.com>
2019-11-27 14:21:36 +0330

    add new template an assets

---
+++ views/blankpage.php
@@ -0,0 +1,18 @@
+<?php
+include_once "views/head.php";		
+include_once "views/logo_header.php";
+include_once "views/navbar.php";
+include_once "views/sidebar.php";		
+		    
+?>
+
+<div class="content">
+		<div class="page-inner">
+			<!-- here goes content -->
+		</div>	
+</div>
+
+	
+<?php
+  include_once "views/footer.php";
+?>
